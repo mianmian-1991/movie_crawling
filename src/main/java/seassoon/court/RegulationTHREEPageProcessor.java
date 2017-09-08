1d8c03e2dafdd5ff4dd8e35042cdc84eba04aee5
叶佩旋 <yepeixuan@seassoon.com>
2017-09-08 18:00:39 +0800

    完成汇法网爬取代码

--- src/main/java/seassoon/court/RegulationTHREEPageProcessor.java
+++ src/main/java/seassoon/court/RegulationTHREEPageProcessor.java
@@ -8,16 +8,26 @@ import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.scheduler.QueueScheduler;
 import us.codecraft.webmagic.scheduler.component.HashSetDuplicateRemover;
 
-public class RegulationTHREEPageProcessor implements PageProcessor{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    String cookie = "ASP.NET_SessionId=fsdakgqxrl12grllmybnxosa; Hm_lvt_3f2eaec16fb0951177798309dd3127b7=1504780457,1504784925; Hm_lpvt_3f2eaec16fb0951177798309dd3127b7=1504784925";
+public class RegulationTHREEPageProcessor implements PageProcessor {
 
-    private Site site = Site.me().addCookie("image.lawxp.com",cookie).setSleepTime(10000).setRetryTimes(1).setRetrySleepTime(10000).setTimeOut(10000);
+    String cookie = "nLoginId=4589763; nLoginTime=2017/9/7 19:48:35; " +
+            "nLoginName=chim_0; ASP.NET_SessionId=5jnq14jciicc3mw5zsmyddqu; " +
+            "tool=; toolsure=; " +
+            "Hm_lvt_3f2eaec16fb0951177798309dd3127b7=1504780457,1504784925,1504853373; " +
+            "Hm_lpvt_3f2eaec16fb0951177798309dd3127b7=1504860641";
+
+    private Site site = Site.me().addCookie("www.lawxp.com", cookie).setSleepTime(6000);
 
 
     String regex_documentUrl = "https://www\\.lawxp\\.com/statute/s\\d+\\.html";
     String regex_documentLink = "/statute/s\\d+\\/.html";
-    String regex_listLink = "\\?pg=\\d+&CourtId=(9759|10000004)";
+//    String regex_listLink = "\\?pg=\\d+&(amp;)*CourtId=(9759|10000004)";
+    String regex_listLink = "\\?pg=\\d+&(amp;)*CourtId=32370";
     String preLink = "https://www.lawxp.com/statute/";
 
     @Override
@@ -28,8 +38,15 @@ public class RegulationTHREEPageProcessor implements PageProcessor{
             page.putField("content", page.getHtml().toString());
         } else {
             //取得链接中符合文档页面或列表页面规则的加入爬取列表
-            page.addTargetRequests(page.getHtml().links().regex(regex_documentLink).all());
-            page.addTargetRequests(page.getHtml().links().regex(regex_listLink).all()); //待测试
+            List<String> listUrls= new ArrayList<>();
+            String content = page.getHtml().toString();
+            Pattern pattern = Pattern.compile(regex_listLink);
+            Matcher matcher = pattern.matcher(content);
+            while (matcher.find()){
+                listUrls.add(preLink+matcher.group());
+            }
+            page.addTargetRequests(page.getHtml().links().regex(regex_documentUrl).all()); //通过测试
+            page.addTargetRequests(listUrls); //通过测试
             page.setSkip(true);
         }
     }
@@ -40,18 +57,21 @@ public class RegulationTHREEPageProcessor implements PageProcessor{
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BasicConfigurator.configure();
 
         String url_guowuyuan = "https://www.lawxp.com/statute/?CourtId=9759";
         String url_renda = "https://www.lawxp.com/statute/?CourtId=10000004";
-        String url_renda_2 = "https://www.lawxp.com/statute/?pg=2&CourtId=10000004";
+        String url_quanguorenda = "https://www.lawxp.com/statute/?CourtId=32370";
 
-        Spider.create(new RegulationTWOPageProcessor())
-                .addUrl(url_renda_2)
-                .addPipeline(new MySQLPipeline("regulations_html_test"))
+//        String url_renda_2 = "https://www.lawxp.com/statute/?pg=2&CourtId=10000004";
+        String url_exam618 = "https://www.lawxp.com/statute/s1784618.html";
+
+        Spider.create(new RegulationTHREEPageProcessor())
+                .addUrl(url_quanguorenda)
+                .addPipeline(new MySQLPipeline("regulations_html_huifawang"))
                 .setScheduler(new QueueScheduler().setDuplicateRemover(new HashSetDuplicateRemover()))
-                .thread(2).run();
+                .thread(4).run();
 
     }
 }
