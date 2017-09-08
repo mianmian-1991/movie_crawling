340ae79d54d8be8cb7eac060fe4153db4f538257
叶佩旋 <yepeixuan@seassoon.com>
2017-09-08 15:38:54 +0800

    reduce sleeptime for TWO create THREE

--- src/main/java/seassoon/court/RegulationTHREEPageProcessor.java
+++ src/main/java/seassoon/court/RegulationTHREEPageProcessor.java
@@ -1,18 +1,37 @@
 package seassoon.court;
 
+import org.apache.log4j.BasicConfigurator;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.scheduler.QueueScheduler;
+import us.codecraft.webmagic.scheduler.component.HashSetDuplicateRemover;
 
 public class RegulationTHREEPageProcessor implements PageProcessor{
 
     String cookie = "ASP.NET_SessionId=fsdakgqxrl12grllmybnxosa; Hm_lvt_3f2eaec16fb0951177798309dd3127b7=1504780457,1504784925; Hm_lpvt_3f2eaec16fb0951177798309dd3127b7=1504784925";
 
-    private Site site = Site.me().addCookie("image.lawxp.com",cookie);
+    private Site site = Site.me().addCookie("image.lawxp.com",cookie).setSleepTime(10000).setRetryTimes(1).setRetrySleepTime(10000).setTimeOut(10000);
+
+
+    String regex_documentUrl = "https://www\\.lawxp\\.com/statute/s\\d+\\.html";
+    String regex_documentLink = "/statute/s\\d+\\/.html";
+    String regex_listLink = "\\?pg=\\d+&CourtId=(9759|10000004)";
+    String preLink = "https://www.lawxp.com/statute/";
 
     @Override
     public void process(Page page) {
-
+        if (page.getUrl().regex(regex_documentUrl).match()) {
+            //如果是文档页面，则存入数据库
+            page.putField("url", page.getUrl().toString());
+            page.putField("content", page.getHtml().toString());
+        } else {
+            //取得链接中符合文档页面或列表页面规则的加入爬取列表
+            page.addTargetRequests(page.getHtml().links().regex(regex_documentLink).all());
+            page.addTargetRequests(page.getHtml().links().regex(regex_listLink).all()); //待测试
+            page.setSkip(true);
+        }
     }
 
     @Override
@@ -22,9 +41,17 @@ public class RegulationTHREEPageProcessor implements PageProcessor{
 
 
     public static void main(String[] args){
+        BasicConfigurator.configure();
 
-        String mainpage = "https://www.lawxp.com/statute/";
+        String url_guowuyuan = "https://www.lawxp.com/statute/?CourtId=9759";
+        String url_renda = "https://www.lawxp.com/statute/?CourtId=10000004";
+        String url_renda_2 = "https://www.lawxp.com/statute/?pg=2&CourtId=10000004";
 
+        Spider.create(new RegulationTWOPageProcessor())
+                .addUrl(url_renda_2)
+                .addPipeline(new MySQLPipeline("regulations_html_test"))
+                .setScheduler(new QueueScheduler().setDuplicateRemover(new HashSetDuplicateRemover()))
+                .thread(2).run();
 
     }
 }
--- src/main/java/seassoon/court/RegulationTWOPageProcessor.java
+++ src/main/java/seassoon/court/RegulationTWOPageProcessor.java
@@ -9,28 +9,24 @@ import us.codecraft.webmagic.scheduler.QueueScheduler;
 import us.codecraft.webmagic.scheduler.component.HashSetDuplicateRemover;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RegulationTWOPageProcessor implements PageProcessor {
 
-    private String regex_document = "http://www\\.chinacourt\\.org/law/detail/\\d+/\\d+/id/\\d+\\.shtml";
+    private String regex_documentUrl = "http://www\\.chinacourt\\.org/law/detail/\\d+/\\d+/id/\\d+\\.shtml";
     private String regex_documentLink = "/law/detail/\\d+/\\d+/id/\\d+\\.shtml";
 
-    private Site site = Site.me().setRetryTimes(1).setTimeOut(7000).setDomain("www.chinacourt.org").setSleepTime(20000).setRetrySleepTime(20000);
+    private Site site = Site.me().setRetryTimes(1).setTimeOut(6000).setDomain("www.chinacourt.org").setSleepTime(12000).setRetrySleepTime(5000);
 
     @Override
     public void process(Page page) {
-        if (page.getUrl().regex(regex_document).match()) {
+        if (page.getUrl().regex(regex_documentUrl).match()) {
             //如果是文档页面，则存入数据
             page.putField("url", page.getUrl().toString());
             page.putField("content", page.getHtml().toString());
         } else {
             //取得链接中符合文档页面规则的加入爬取列表
-            List<String> urls = page.getHtml().links().regex(regex_documentLink).all();
-            System.out.println(urls.toArray());
-            page.addTargetRequests(urls);
-//            page.addTargetRequests(page.getHtml().links().regex(regex_documentLink).all());
+            page.addTargetRequests(page.getHtml().links().regex(regex_documentLink).all());
             page.setSkip(true);
         }
     }
@@ -51,13 +47,15 @@ public class RegulationTWOPageProcessor implements PageProcessor {
         List<String> urlList = new ArrayList<>();
         int page;
         //添加国家法律法规的列表页
-        for (page = 1; page <= 100; page++) {
-            urlList.add("http://www.chinacourt.org/law/more/law_type_id/MzAwNEAFAA%3D%3D/page/" + page + ".shtml");
-        }
-        //添加司法解释的列表页
-//        for (page = 1; page <= 4; page++) {
-//            urlList.add("http://www.chinacourt.org/law/more/law_type_id/MzAwM0AFAA%3D%3D/page/" + page + ".shtml");
+//        for (page = 101; page <= 200; page++) {
+//            urlList.add("http://www.chinacourt.org/law/more/law_type_id/MzAwNEAFAA%3D%3D/page/" + page + ".shtml");
 //        }
+
+        //添加司法解释的列表页
+        for (page = 1; page <= 4; page++) {
+            urlList.add("http://www.chinacourt.org/law/more/law_type_id/MzAwM0AFAA%3D%3D/page/" + page + ".shtml");
+        }
+
         //添加地方法规的列表页
 //        for (page = 1; page <= 1175; page++) {
 //            urlList.add("http://www.chinacourt.org/law/more/law_type_id/MzAwMkAFAA%3D%3D/page/" + page + ".shtml");
